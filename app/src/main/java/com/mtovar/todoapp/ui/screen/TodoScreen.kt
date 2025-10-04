8708159fd599fe89697be09eb8159c6577cd94c7
Marglodis Tovar <marglodistovar@gmail.com>
2025-10-04 12:35:06 -0300

    Snackbar al elimnar tarea

--- app/src/main/java/com/mtovar/todoapp/ui/screen/TodoScreen.kt
+++ app/src/main/java/com/mtovar/todoapp/ui/screen/TodoScreen.kt
@@ -11,17 +11,25 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.SnackbarResult
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -30,56 +38,86 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.mtovar.todoapp.model.TodoItem
 import com.mtovar.todoapp.viewmodel.TodoViewModel
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
 @Composable
 fun TodoScreen(viewModel: TodoViewModel) {
-    val todoItems = viewModel.todoItems.collectAsState()
-    var newTodoTitle by remember { mutableStateOf("") }
+    val todoItems by viewModel.todoItems.collectAsState()
+    var newTask by remember { mutableStateOf("") }
 
+    val snackbarMessage by viewModel.snackbarMessage.collectAsState()
+    val coroutineScope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState() }
 
-    Column(modifier = Modifier
-        .fillMaxSize()
-        .statusBarsPadding()
-        .padding(16.dp)) {
-        Text(
-            text = "Lista de Tareas",
-            modifier = Modifier.padding(bottom = 16.dp),
-            style = MaterialTheme.typography.headlineMedium
-        )
-
-        // Input field and ADD Buttton
-        Row(modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
-            TextField(
-                value = newTodoTitle,
-                onValueChange = { newTodoTitle = it },
-                modifier = Modifier.weight(1f),
-                label = { Text("Nueva tarea") }
-            )
-            Spacer(modifier = Modifier.width(8.dp))
-            Button(
-                onClick = {
-                    if (newTodoTitle.isNotBlank()) {
-                        viewModel.addTodo(newTodoTitle)
-                        newTodoTitle = ""
-                    }
-                }
-
-            ) {
-                Text("Agregar")
-            }
-        }
-        Spacer(modifier = Modifier.height(16.dp))
-
-        // Todo list
-        LazyColumn {
-            items(todoItems.value.size) { item ->
-                TodoItemRow(
-                    item = todoItems.value[item],
-                    onToggleComplete = { id -> viewModel.toggleTodo(id) },
-                    onDelete = { id -> viewModel.deleteTodo(id) }
+    // Lanzar el efecto para mostrar el snackbar cunaod se emite un mensaje
+    LaunchedEffect(snackbarMessage) {
+        snackbarMessage?.let {
+            message ->
+            coroutineScope.launch {
+                val result = snackbarHostState.showSnackbar(
+                    message = message,
+                    actionLabel = "Deshacer",
+                    duration = SnackbarDuration.Short
                 )
+                if (result == SnackbarResult.ActionPerformed) {
+                    viewModel.undoDelete()
+                }
+                viewModel.clearSnackbarMessage()
             }
         }
+    }
+
+    Scaffold(
+        snackbarHost = { SnackbarHost(snackbarHostState) },
+        modifier = Modifier.fillMaxSize()
+    ) {
+        padding ->
+        Column(modifier = Modifier
+            .fillMaxSize()
+            .statusBarsPadding()
+            .padding(16.dp)) {
+            Text(
+                text = "Lista de Tareas",
+                modifier = Modifier.padding(bottom = 16.dp),
+                style = MaterialTheme.typography.headlineMedium
+            )
+
+            // Input field and ADD Buttton
+            Row(modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
+                TextField(
+                    value = newTask,
+                    onValueChange = { newTask = it },
+                    modifier = Modifier.weight(1f),
+                    label = { Text("Nueva tarea") }
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                Button(
+                    onClick = {
+                        if (newTask.isNotBlank()) {
+                            viewModel.addTodo(newTask)
+                            newTask = ""
+                        }
+                    }
+
+                ) {
+                    Text("Agregar")
+                }
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+
+            // Todo list
+            LazyColumn {
+                items(todoItems) { item ->
+                    TodoItemRow(
+                        item = item,
+                        onToggleComplete = { id -> viewModel.toggleComplete(id) },
+                        onDelete = { id -> viewModel.deleteTodo(id) }
+                    )
+                }
+            }
+        }
+
     }
 }
 
--- app/src/main/java/com/mtovar/todoapp/viewmodel/TodoViewModel.kt
+++ app/src/main/java/com/mtovar/todoapp/viewmodel/TodoViewModel.kt
@@ -10,13 +10,18 @@ class TodoViewModel : ViewModel() {
     private val _todoItems = MutableStateFlow<List<TodoItem>>(emptyList())
     val todoItems: StateFlow<List<TodoItem>> = _todoItems.asStateFlow()
 
+    private val _snackbarMessage = MutableStateFlow<String?>(null)
+    val snackbarMessage: StateFlow<String?> = _snackbarMessage.asStateFlow()
+
     private var nextId = 0
+    private var lastDeletedItem: TodoItem? = null
+
 
     fun addTodo(title: String) {
-        _todoItems.value = _todoItems.value + TodoItem(id = nextId++, title = title, false)
+        _todoItems.value = _todoItems.value + TodoItem(id = ++nextId, title = title)
     }
 
-    fun toggleTodo(id: Int) {
+    fun toggleComplete(id: Int) {
         _todoItems.value = _todoItems.value.map {
             if (it.id == id)
                 it.copy(isCompleted = !it.isCompleted) else it
@@ -25,6 +30,24 @@ class TodoViewModel : ViewModel() {
     }
 
     fun deleteTodo(id: Int) {
-        _todoItems.value = _todoItems.value.filter { it.id != id }
+        val itemToDelete = _todoItems.value.find { it.id == id }
+        itemToDelete?.let {
+            lastDeletedItem = it
+            _todoItems.value = _todoItems.value.filter { it.id != id }
+            _snackbarMessage.value = "Tarea eliminada: ${it.title}"
+         }
+
+        }
+
+    fun undoDelete(){
+        lastDeletedItem?.let { item ->
+            _todoItems.value = (_todoItems.value + item).sortedBy { it.id }
+            lastDeletedItem = null
+        }
+    }
+
+    fun clearSnackbarMessage() {
+        _snackbarMessage.value = null
+        lastDeletedItem = null
     }
 }
